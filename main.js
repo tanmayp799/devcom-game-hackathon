16aab956a7175e7bcf78ce5088f7adfbf39398b7
DheGit <bankerdheer@gmail.com>
2022-01-25 12:57:29 +0530

    added features to Ball and tested by drawing on canvas

--- main.js
+++ main.js
@@ -6,25 +6,47 @@ class Canvas2D {
 
 	init(canvasId){
 		this.canvasObject = document.getElementById(canvasId);
-		this.canvasWriter = this.canvas.getContext("2d");
+		this.canvasWriter = this.canvasObject.getContext("2d");
 	}
 
-	drawImg(img) {
-		this.canvasWriter.drawImage(img, img.width, img.height);
+	drawImg(img, pos, dimen) {
+		this.canvasWriter.drawImage(img, pos.x, pos.y, dimen.width, dimen.height);
 	}
 }
 
 class Ball {
-	constructor(_x,_y,_radius, _vx, _vy){
-		this.x = _x;
-		this.y = _y;
+	constructor(_pos,_radius, _vx, _vy){
+		//this.x and this.y are the coordinates of the center of the ball
+		this.x = _pos.x;
+		this.y = _pos.y;
 		this.radius = _radius;
 		this.vx = _vx;
 		this.vy = _vy;
+
+		this.selfImg = null;
+	}
+
+	setSelfImgById(id){
+		this.selfImg = document.getElementById(id);
+	}
+
+	getDimension(){
+		return {width: 2 * this.radius, height : 2 * this.radius};
+	}
+	getCornerPosition(){ //Utility function to get the coordinates of the top-left corner of the ball (because that's the position used by canvas for drawing)
+		return {x: this.x - this.radius, y: this.y - this.radius};
+	}
+	getPosition(){
+		return {x: this.x, y: this.y};
 	}
 }
 
 canvas = new Canvas2D();
 canvas.init("gameCanvas");
 
-// canvas.drawImg(document.getElementById("8ball"));
+ball_8 = new Ball({x:0, y:0}, 30, 0, 0); //If all works fine, this should draw a quarter of the 8-ball, centered at the top-left corner of the canvas
+
+window.onload = function(){
+	ball_8.setSelfImgById("ball8");
+	canvas.drawImg(ball_8.selfImg, ball_8.getCornerPosition(), ball_8.getDimension());
+};
