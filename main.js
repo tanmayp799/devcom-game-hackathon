24e6ed9990d6bb73b198a7940ae1944925ad4e94
DheGit <bankerdheer@gmail.com>
2022-01-25 10:02:33 +0530

    made a class for Canvas and tried drawing an image on canvas

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-canvasObject = document.getElementById("gameCanvas");
-canvasWriter = canvasObject.getContext("2d");
+class Canvas2D {
+	constructor(){
+		this.canvasObject = null;
+		this.canvasWriter = null;
+	}
+
+	init(canvasId){
+		this.canvasObject = document.getElementById(canvasId);
+		this.canvasWriter = this.canvas.getContext("2d");
+	}
+
+	drawImg(img) {
+		this.canvasWriter.drawImage(img, img.width, img.height);
+	}
+}
 
 class Ball {
 	constructor(_x,_y,_radius, _vx, _vy){
@@ -10,3 +23,8 @@ class Ball {
 		this.vy = _vy;
 	}
 }
+
+canvas = new Canvas2D();
+canvas.init("gameCanvas");
+
+// canvas.drawImg(document.getElementById("8ball"));
