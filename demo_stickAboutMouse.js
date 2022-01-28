2358945c92a1857f365653ebb8abb0a9590a4bb1
tanmayp799 <tanmayp799@gmail.com>
2022-01-28 18:32:21 +0530

    Added margin change function

--- demo_stickAboutMouse.js
+++ demo_stickAboutMouse.js
@@ -2,12 +2,15 @@ let canvas = new Canvas2D();
 canvas.init("gameCanvas");
 canvas.setDimens({width: D_CANVAS_W, height: D_CANVAS_H});
 
+let cueStick = new CueStick({x: 300, y: 300}, 50, {width: D_CUE_L, height:D_CUE_B});
+
 let img = new Image();
 img.src = P_CUESTICK;
+img.id = "cueStick";
 
 imgPos = {x:100, y:50};
 imgDimens = {width: D_CUE_L, height:D_CUE_B};
-rPos = {x:300, y:60};
+rPos = {x:imgPos.x + 50 + D_CUE_L, y: imgPos.y + 50 + D_CUE_B/2};
 
 function updateStick(event){
 	canvas.clear();
